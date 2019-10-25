77b5426ac6b26b0ec5c51352e78eaabbc85840dd
atcotton <39640879+atcotton@users.noreply.github.com>
2019-10-25 17:55:37 -0600

    Update script.js
    Got the Math.js api working

--- script.js
+++ script.js
@@ -17,56 +17,38 @@ var app = new Vue({
     initilize:function(){
       console.log("I'm alive. Just tidying up here.");
       
-      
-      /*
-      var expr = {
-    "expr": [
-      "a = 1.2 * (2 + 4.5)",
-      "a / 2",
-      "5.08 cm in inch",
-      "sin(45 deg) ^ 2",
-      "9 / 3 + 2i",
-      "b = [-1, 2; 3, 1]",
-      "det(b)"
-    ],
-    "precision": 14
-  }
-
-  .ajax({
-    type: 'POST',
-    url: '/v4/',
-    data: expr,
-    contentType: 'text/plain',
-    success: function (result) {
-      alert(JSON.stringify(result, null, 2))
-    },
-    error: function (resp) {
-      alert('Error: ' + resp.responseText)
-    }
-  })*/
-      
-      //fetch('http://api.mathjs.org/v4/?expr=2*(7-3)')
-      //.then((resp) => this.answer1)
-      
-      /*
-      fetch('http://api.mathjs.org/v4/?expr=2*(7-3)')
-.then((res) => {
-      console.log(res.text());
-      return res.text();
-})
-.then((data) => this.answer1);*//*{
-     this.answer1.html(data);
-});*/
-      /*
-      .then(function(data){
-        console.log(data.result);
-        let answer1b = data.result;
-      });*/
+      axios.get('http://api.mathjs.org/v4/?expr=7%2B8*2')
+        .then(response => {
+          this.answer1 = response.data;
+          console.log("the 1 beacons are lit! " + this.answer1);
+          return true;
+        })
+        .catch(error => {
+          console.log(error)
+        });
         
         
-      this.answer1 = 23;
-      this.answer2 = 15;
-      this.answer3 = 9;
+      axios.get('http://api.mathjs.org/v4/?expr=2%5E3%2B7')
+        .then(response => {
+          this.answer2 = response.data;
+          console.log("the 2 beacons are lit! " + this.answer2);
+          return true;
+        })
+        .catch(error => {
+          console.log(error)
+        });
+        
+        
+      axios.get('http://api.mathjs.org/v4/?expr=27%2F3')
+        .then(response => {
+          this.answer3 = response.data;
+          console.log("the 3 beacons are lit! " + this.answer3);
+          return true;
+        })
+        .catch(error => {
+          console.log(error)
+        });
+      
       
       
     },
@@ -92,24 +74,7 @@ var app = new Vue({
       } else{
         console.log("nah");
       }
-      //var url = "http://bioresearch.byu.edu/cs260/jquery/getcity.cgi?q=" + this.prefix;
-      /*
-      console.log("URL " + url);
-      fetch(url)
-        .then((data) => {
-          return (data.json());
-        })
-        .then((citylist) => {
-          console.log("CityList");
-          console.log(citylist);
-          this.cities = [];
-          for (let i = 0; i < citylist.length; i++) {
-            console.log(citylist[i].city);
-            this.cities.push({ name: citylist[i].city });
-          };
-          console.log("Got Citylist");
-        });
-        */
+     
     },
   },
   created: function(){
